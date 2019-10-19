43f7e3f927aa39bfbc2364694e418e8fc5697bec
Aos <aosdab@gmail.com>
2019-10-18 23:59:52 -0400

    Added skeleton of parser

--- bf.go
+++ bf.go
@@ -1,34 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-// Example bracket configurations
-// TODO: Turn this into a test
-var examples = map[string]bool{
-	"[]":         true,
-	"][":         false,
-	"[[[][]][]]": true,
-	"[[][]]":     true,
-	"[[]":        false,
-	"][[]":       false,
-}
+// The main loop is a switch/case statement parsing each character by its
+// character code. The character codes are:
+//
+//	+	43
+//	-	45
+//	>	62
+//	<	60
+//	[	91
+//	]	93
+//	.	46
+//	,	44
+//
+
+const example = ">><-++].,"
 
 func main() {
-	for example, ok := range examples {
-		// Bracket parser
-		var s []int
-		for i, c := range example {
-			switch {
-			case c == 91:
-				s = append(s, i)
-			case c == 93 && len(s) > 0:
-				// Pop from slice
-				_, s = s[len(s)-1], s[:len(s)-1]
-			}
-		}
+	var s []int // bracket stack
 
-		if (len(s) == 0) != ok {
-			fmt.Println(example)
+	for i, c := range example {
+		switch c {
+		// +
+		case 43:
+			fmt.Println("+")
+		// -
+		case 45:
+			fmt.Println("-")
+		// >
+		case 62:
+			fmt.Println(">")
+		// <
+		case 60:
+			fmt.Println("<")
+		// .
+		case 46:
+			fmt.Println(".")
+		// ,
+		case 44:
+			fmt.Println(",")
+		// [
+		case 91:
+			s = append(s, i)
+		// ]
+		case 93:
+			if len(s) <= 0 {
+				log.Fatalf("ERROR: Mismatched brackets, index: %d", i)
+			}
+
+			// Pop from slice
+			_, s = s[len(s)-1], s[:len(s)-1]
 		}
+	}
+
+	if len(s) > 0 {
+		log.Fatal("ERROR: Mismatched brackets")
 	}
 }
--- brackets_test.go
+++ brackets_test.go
@@ -22,29 +22,26 @@ var tests = []struct {
 }
 
 func TestBracketParser(t *testing.T) {
-	t.Log("Given the need to teset different bracket configurations...")
-	{
-		for num, tt := range tests {
-			t.Logf("\tTest %d:\t%v", num, tt.configuration)
-			{
-				// Example bracket parser
-				var s []int
-				for i, c := range tt.configuration {
-					switch {
-					case c == 91:
-						s = append(s, i)
-					case c == 93 && len(s) > 0:
-						_, s = s[len(s)-1], s[:len(s)-1]
-					}
+	for num, tt := range tests {
+		t.Logf("\tTest %d:\t%v", num, tt.configuration)
+		{
+			// Example bracket parser
+			var s []int
+			for i, c := range tt.configuration {
+				switch {
+				case c == 91:
+					s = append(s, i)
+				case c == 93 && len(s) > 0:
+					_, s = s[len(s)-1], s[:len(s)-1]
 				}
-
-				if (len(s) == 0) != tt.correct {
-					t.Errorf("\t%s should be %v.", failed, tt.correct)
-				} else {
-					t.Logf("\t%s should be %v.", succeed, tt.correct)
-				}
-
 			}
+
+			if (len(s) == 0) != tt.correct {
+				t.Errorf("\t%s should be %v.", failed, tt.correct)
+			} else {
+				t.Logf("\t%s should be %v.", succeed, tt.correct)
+			}
+
 		}
 	}
 }
