d9053fd0630624e33ff2dbc0e2fa4a1e0d9463ba
Aos <aosdab@gmail.com>
2019-10-26 23:10:53 -0400

    Cleaned up structure, WIP: figure out how to do tracing without affecting runtime

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"regexp"
 )
 
 // The main loop is a switch/case statement parsing each character by its
@@ -21,123 +19,29 @@ import (
 //	,	44 - input
 //
 
-const tapeSize = 30000
+// TAPESIZE is the size of our tape
+const TAPESIZE = 30000
 
-var commands []byte
+var flagTrace bool
+var program *Program
 
 func init() {
-	if len(os.Args) <= 1 || len(os.Args) > 2 {
-		fmt.Println("USAGE: ./gofk <file-name>")
-		os.Exit(0)
-	}
+	flag.BoolVar(&flagTrace,
+		"trace",
+		false,
+		"Traces the number of instructions",
+	)
+	flag.Parse()
+	args := flag.Args()
 
-	openFile, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Println(err)
+	if len(args) == 0 || len(args) > 1 {
+		fmt.Println("USAGE: ./gofk [--trace] <file-name>")
 		os.Exit(1)
 	}
 
-	// Parse out commands
-	// This is the lazy person's approach ;-)
-	r := regexp.MustCompile("[^\\[\\]\\+-\\.,><]")
-	commands = r.ReplaceAllLiteral(openFile, []byte(""))
+	program = ParseProgram(args[0])
 }
 
 func main() {
-	tape := make([]byte, tapeSize)
-	ip := 0 // instruction pointer
-	dp := 0 // data pointer
-	input := bufio.NewReader(os.Stdin)
-	jumpTable := computeJumptable(commands)
-
-	for ip < len(commands) {
-		c := commands[ip]
-
-		switch c {
-		// +
-		case 43:
-			tape[dp]++
-		// -
-		case 45:
-			tape[dp]--
-		// >
-		// Loop around if at end
-		case 62:
-			if dp == tapeSize-1 {
-				dp = 0
-			} else {
-				dp++
-			}
-		// <
-		case 60:
-			if dp == 0 {
-				dp = tapeSize - 1
-			} else {
-				dp--
-			}
-		// .
-		case 46:
-			fmt.Printf("%c", tape[dp])
-		// ,
-		case 44:
-			c, err := input.ReadByte()
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			tape[dp] = c
-		// [
-		case 91:
-			if tape[dp] == 0 {
-				ip = jumpTable[ip]
-			}
-		// ]
-		case 93:
-			if tape[dp] != 0 {
-				ip = jumpTable[ip]
-			}
-		default:
-			fmt.Printf("Error: bad character, ip: %d", ip)
-			os.Exit(1)
-		}
-
-		ip++
-	}
-}
-
-func computeJumptable(commands []byte) []int {
-	table := make([]int, len(commands))
-	ip := 0
-
-	for ip < len(commands) {
-		if commands[ip] == 91 {
-			nestedBracket := 1
-			seek := ip + 1
-
-			for nestedBracket > 0 && seek < len(commands) {
-				switch commands[seek] {
-				case 91:
-					nestedBracket++
-				case 93:
-					nestedBracket--
-				}
-
-				if nestedBracket == 0 {
-					table[ip] = seek
-					table[seek] = ip
-					break
-				}
-
-				seek++
-			}
-
-			if seek > len(commands) {
-				fmt.Println("Error: Mismatched brackets")
-				os.Exit(1)
-			}
-		}
-		ip++
-	}
-
-	return table
+	Interpret(program)
 }
---
+++ program.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+)
+
+// Program is our brainfuck program holding the instructions
+type Program struct {
+	instructions []byte
+}
+
+// ParseProgram takes in the bf filename and returns the program removing
+// all non-supported characters (everything except +-><.,[])
+func ParseProgram(filename string) *Program {
+	openFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Parse out instructions
+	// This is the lazy person's approach ;-)
+	r := regexp.MustCompile("[^\\[\\]\\+-\\.,><]")
+	return &Program{
+		r.ReplaceAllLiteral(openFile, []byte("")),
+	}
+}
+
+// Interpret takes in the program and interprets it
+// This is where the magic happens
+func Interpret(p *Program) {
+	tape := make([]byte, TAPESIZE)
+	ip := 0 // instruction pointer
+	dp := 0 // data pointer
+	input := bufio.NewReader(os.Stdin)
+	jumpTable := computeJumptable(p.instructions)
+
+	for ip < len(p.instructions) {
+		c := p.instructions[ip]
+
+		switch c {
+		// +
+		case 43:
+			tape[dp]++
+		// -
+		case 45:
+			tape[dp]--
+		// >
+		// Loop around if at end
+		case 62:
+			if dp == TAPESIZE-1 {
+				dp = 0
+			} else {
+				dp++
+			}
+		// <
+		case 60:
+			if dp == 0 {
+				dp = TAPESIZE - 1
+			} else {
+				dp--
+			}
+		// .
+		case 46:
+			fmt.Printf("%c", tape[dp])
+		// ,
+		case 44:
+			c, err := input.ReadByte()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			tape[dp] = c
+		// [
+		case 91:
+			if tape[dp] == 0 {
+				ip = jumpTable[ip]
+			}
+		// ]
+		case 93:
+			if tape[dp] != 0 {
+				ip = jumpTable[ip]
+			}
+		default:
+			fmt.Printf("Error: bad character, ip: %d", ip)
+			os.Exit(1)
+		}
+
+		ip++
+	}
+}
+
+func computeJumptable(instructions []byte) []int {
+	table := make([]int, len(instructions))
+	ip := 0
+
+	for ip < len(instructions) {
+		if instructions[ip] == 91 {
+			nestedBracket := 1
+			seek := ip + 1
+
+			for nestedBracket > 0 && seek < len(instructions) {
+				switch instructions[seek] {
+				case 91:
+					nestedBracket++
+				case 93:
+					nestedBracket--
+				}
+
+				if nestedBracket == 0 {
+					table[ip] = seek
+					table[seek] = ip
+					break
+				}
+
+				seek++
+			}
+
+			if seek > len(instructions) {
+				fmt.Println("Error: Mismatched brackets")
+				os.Exit(1)
+			}
+		}
+		ip++
+	}
+
+	return table
+}
